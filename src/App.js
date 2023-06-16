c278027fc2ad4f2592872ed98d914a2016603201
liz <lissete.trejo@gmail.com>
2023-06-16 12:44:56 -0700

    wave 01 - update ChatEntry component to take in id, sender, body, timeStamp, and liked props. add logic for likedText and likedClass. add ChatEntry propTypes. pass Wave 01 tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry.js';
 
 const App = () => {
+  const { sender, body, timeStamp, id, liked } = chatMessages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <section className="chatlog">
+          <ChatEntry 
+            id={id} 
+            sender={sender} 
+            body={body} 
+            timeStamp={timeStamp} 
+            liked={liked} 
+          />
+        </section>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({id, sender, body, timeStamp, liked}) => {
+  const likedText = liked ? '❤️' : '🤍';
+  const likedClass = liked ? 'liked' : 'not-liked';
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{body}</p>
+        <p className="entry-time">
+          <TimeStamp time={timeStamp} />
+        </p>
+        <button className="like">{likedText}</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
